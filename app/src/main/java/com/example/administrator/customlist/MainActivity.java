22276d58f8bdb1a4ddcf0f6770321645f3b4386d
Akshay Mathur <akshaymathur1992@gmail.com>
2015-11-16 18:12:54 +0530

    added comment to MainActivity.java

--- app/src/main/java/com/example/administrator/customlist/MainActivity.java
+++ app/src/main/java/com/example/administrator/customlist/MainActivity.java
@@ -19,7 +19,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//comment added by ----
 
 public class MainActivity extends AppCompatActivity {
 
